fc43282fa2c9331ed6ee70ddd46423557a598d9b
Anuj Tyagi <tyagi.an@northeastern.edu>
2026-06-29 21:43:15 -0400

    version bump

--- sentinelguard/__init__.py
+++ sentinelguard/__init__.py
@@ -36,7 +36,7 @@ Usage:
     guard = SentinelGuard(config=config)
 """
 
-__version__ = "0.0.6"
+__version__ = "0.0.7"
 __author__ = "SentinelGuard Contributors"
 
 from sentinelguard.core.guard import SentinelGuard
